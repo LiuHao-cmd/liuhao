393526569a733c66a33c3d1aa09ff343443c7703
liuhao-cmd <ishenhaixingkongi@163.com>
2021-06-07 08:51:51 +0800

    blog项目初始化目录结构

---
+++ liuhaoblog/src/main/java/com/liuhao/blog/BlogApplication.java
@@ -0,0 +1,16 @@
+package com.liuhao.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: liuhao
+ * @Date: 2021/6/4 10:12
+ * @Description
+ */
+@SpringBootApplication
+public class BlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class,args);
+    }
+}
