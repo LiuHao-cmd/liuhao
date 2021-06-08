31fd911ac13cb81fc777137e82d1628980ad8a1c
liuhao-cmd <ishenhaixingkongi@163.com>
2021-06-08 08:37:42 +0800

    程序打成war包

--- liuhaoblog/src/main/java/com/liuhao/blog/BlogApplication.java
+++ liuhaoblog/src/main/java/com/liuhao/blog/BlogApplication.java
@@ -2,6 +2,8 @@ package com.liuhao.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * @Author: liuhao
@@ -9,8 +11,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Description
  */
 @SpringBootApplication
-public class BlogApplication {
+public class BlogApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(BlogApplication.class,args);
+    }
+
+    /**
+     * 重写servlet初始化器
+     * @param builder
+     * @return
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BlogApplication.class);
     }
 }
